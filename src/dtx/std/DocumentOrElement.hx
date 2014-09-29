ceb763c611e740a8dceb8741062b3dd9904a9124
Skial Bainn <skial.bainn@gmail.com>
2014-09-29 13:57:30 +0100

    Remove type param.

--- src/dtx/std/DocumentOrElement.hx
+++ src/dtx/std/DocumentOrElement.hx
@@ -4,11 +4,11 @@ package dtx.std;
  * ...
  * @author Skial Bainn
  */
-abstract DocumentOrElement<T>(T) to T {
-	inline function new(n:T) {
+abstract DocumentOrElement(DOMNode) to DOMNode {
+	inline function new(n:DOMNode) {
 		this = n;
 	}
 
 	/** On non-JS platforms, all nodes are Xml objects, and all can be used with the `selecthxml` selector engine, so accept casts from all nodes. **/
-	@:from static inline function fromXml( x:T ) return new DocumentOrElement<T>( x );
+	@:from static inline function fromXml( x:DOMNode ) return new DocumentOrElement( x );
 }
